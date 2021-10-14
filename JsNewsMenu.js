fb1990a02a0c45390688c04b786ef2b50fc0d96f
School10-Konkurs <92534792+School10-Konkurs@users.noreply.github.com>
2021-10-14 18:12:45 +0300

    Add files via upload

---
+++ JsNewsMenu.js
@@ -0,0 +1,8 @@
+function menu_open(){
+	document.getElementById('menulist').style.left= "270px";
+	console.log("1");
+}
+function menu_close(){
+	document.getElementById('menulist').style.left= "0";
+	console.log("0");
+}
